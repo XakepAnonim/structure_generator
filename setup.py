c297bc0b296aad25b90aac5f2fa7f7e3433cdcf5
XakepAnonim <dimka.fisenko@mail.ru>
2024-11-06 10:12:58 +0300

    0.2.0 (#2)

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="structure-generator-by-xakepanonim",
-    version="0.1.0",
+    version="0.2.0",
     author="XakepAnonim",
     description="Библиотека для генерации описания структуры проекта в README.md",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/XakepAnonim/structure_generator",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['toml'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
